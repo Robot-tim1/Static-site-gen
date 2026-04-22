721dad26d4f2c4b8dbfbddebee4817c58d1cfd04
Robot-tim1 <memopada8@gmail.com>
2026-04-23 01:14:45 +0700

    copy from static to public function done

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 import os
+import shutil
 
 from textnode import *
 from htmlnode import *
 from page_generation import *
 
 def main():
+    if os.path.exists('public'):
+        shutil.rmtree('public')
+        os.mkdir('public')
+    else:
+        os.mkdir('public')
     static_to_public()
 
 main()
--- src/page_generation.py
+++ src/page_generation.py
@@ -1,4 +1,21 @@
 import os
+import shutil
 
 def static_to_public(static_dir=os.path.join('.', 'static'), public_dir=os.path.join('.', 'public'), next_dir=None):
-    pass
+    if next_dir:
+        static_dir = os.path.join(static_dir, next_dir)
+        public_dir = os.path.join(public_dir, next_dir)
+    directory_list = []
+
+    static_list = os.listdir(static_dir)
+    for file in static_list:
+        file_static = os.path.join(static_dir, file)
+        file_public = os.path.join(public_dir, file)
+        if os.path.isfile(file_static):
+            shutil.copy(file_static, public_dir)
+        else:
+            os.mkdir(file_public)
+            directory_list.append(file)
+
+    for directory in directory_list:
+        static_to_public(static_dir, public_dir, directory)
